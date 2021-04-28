f7088be8c501d617cbcac1423319e711b03bbe07
Mohd Shaib <shaj.native@gmail.com>
2021-04-28 14:07:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import Input from "./Input";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Input />
+    </div>
+  );
+}
